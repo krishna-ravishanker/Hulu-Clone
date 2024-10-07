a65bbd4bcb7780163b168e8a8f95d12ca0f31cb4
krishna-ravishanker <114725937+krishna-ravishanker@users.noreply.github.com>
2024-10-07 19:55:01 +0530

    First commit

---
+++ dashboard.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="dash.css">
+    <title>Dashboard</title>
+</head>
+<body>
+<header class="header">
+    <img src="img/logo.png" id="hulu-logo" alt="logo"> <nobr>
+      <nav>
+        <ul>
+          <li>
+            <button class="login-btn"><a href="./registration.php">Account</a></button>
+          </li>
+        </ul>
+      </nav>
+      <div class="header-content">
+        <h4>Welcome to the dashboard</h4>
+        <img src="img/logo.png" alt="Hulu" class="logo" />
+        <div class="header-text-1">Watch thousands of TV shows and movies.</div>
+        <div class="header-text-2">
+          HBO Max™, SHOWTIME®, CINEMAX® and STARZ® available as add-ons.
+        </div>
+        <a href="#home"><button class="btn btn-cta">Start Watching</button></a>
+      </div>
+</header>
+<section class="main-container" >
+      <div class="location" id="home">
+          <h1 id="home">Popular on Hulu</h1>
+          <div class="box">
+            <a href=""><img src="https://raw.githubusercontent.com/krishna-ravishan/images/main/1.jpg" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images.jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images%20(1).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images%20(2).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(8).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(6).jpg?raw=true" alt=""></a>
+    
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(5).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(4).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(3).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(2).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(1).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(7).jpg?raw=true" alt=""></a>        
+          </div>
+      </div>
+</section>
+<section class="main-container" >
+      <div class="location" id="home">
+          <h1 id="home">Trending</h1>
+          <div class="box">
+            <a href=""><img src="https://raw.githubusercontent.com/krishna-ravishan/images/main/1.jpg" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images.jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images%20(1).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images%20(2).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(8).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(6).jpg?raw=true" alt=""></a>
+    
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(5).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(4).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(3).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(2).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(1).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(7).jpg?raw=true" alt=""></a>        
+          </div>
+      </div>
+</section>
+<section class="main-container" >
+      <div class="location" id="home">
+          <h1 id="home">English</h1>
+          <div class="box">
+            <a href=""><img src="https://raw.githubusercontent.com/krishna-ravishan/images/main/1.jpg" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images.jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images%20(1).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/images%20(2).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(8).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(6).jpg?raw=true" alt=""></a>
+    
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(5).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(4).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(3).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(2).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(1).jpg?raw=true" alt=""></a>
+            <a href=""><img src="https://github.com/krishna-ravishan/images/blob/main/download%20(7).jpg?raw=true" alt=""></a>        
+          </div>
+      </div>
+</section>
+</body>
+</html>
